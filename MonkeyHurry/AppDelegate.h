4a5929157e2ce7c92d1380924633b77cda2ba4d2
tough <363652003@qq.com>
2018-11-12 23:46:46 +0800

    Initial Commit

---
+++ MonkeyHurry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonkeyHurry
+//
+//  Created by tough on 2018/11/12.
+//  Copyright © 2018年 tough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
